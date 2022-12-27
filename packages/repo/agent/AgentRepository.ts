b1973b89e937f2aa8e6f05f08719526535299296
laibavi <017laibavi.wts@gmail.com>
2022-12-27 16:36:39 +0700

    code core repo

---
+++ packages/repo/agent/AgentRepository.ts
@@ -0,0 +1,59 @@
+import { createRepository } from "@moose-desk/core";
+import env from "../env";
+import {
+  ActiveNewAgentRequest,
+  ActiveNewAgentResponse,
+  CreateAgentRequest,
+  CreateAgentResponse,
+  DeActiveAgentResponse,
+  DeleteAgentResponse,
+  GetListAgentRequest,
+  GetListAgentResponse,
+  GetOneAgentResponse,
+  ReActiveResponse,
+  ResendEmailInvitationRequest,
+  ResendEmailInvitationResponse,
+  UpdateAgentRequest,
+  UpdateAgentResponse,
+} from "./Agent";
+
+export const AgentRepository = createRepository(
+  {
+    baseURL: `${env.API_URL}/api/v1/account/agent`,
+  },
+  {
+    getList(api, params: GetListAgentRequest) {
+      return api.get<GetListAgentResponse>("", params);
+    },
+    getOne(api, id: string) {
+      return api.get<GetOneAgentResponse>(`/${id}`);
+    },
+    create(api, data: CreateAgentRequest) {
+      return api.post<CreateAgentResponse>("", data);
+    },
+    update(api, id: string, data: UpdateAgentRequest) {
+      return api.put<UpdateAgentResponse>(`/${id}`, data);
+    },
+    delete(api, id: string) {
+      return api.delete<DeleteAgentResponse>(`/${id}`);
+    },
+
+    activeNewAgent(api, data: ActiveNewAgentRequest) {
+      return api.put<ActiveNewAgentResponse>("/active-new-agent", data);
+    },
+
+    resendEmailInvitation(api, data: ResendEmailInvitationRequest) {
+      return api.put<ResendEmailInvitationResponse>("/resend-invitation", data);
+    },
+
+    deActiveAgent(api, id: string) {
+      return api.put<DeActiveAgentResponse>(`/deactive/${id}`, {});
+    },
+
+    reActiveAgent(api, id: string) {
+      return api.put<ReActiveResponse>(`/reactive/${id}`, {});
+    },
+  }
+);
+
+export default AgentRepository;
---
+++ packages/repo/env.ts
@@ -0,0 +1,5 @@
+const env = {
+  API_URL: "https://api.moosedesk.net",
+};
+
+export default env;
