13780bf6b0bc897fe029836b4e70950008b0c4c4
laibavi <017laibavi.wts@gmail.com>
2023-01-03 15:33:13 +0700

    redirect auth

--- apps/standalone/src/pages/RedirectPage.tsx
+++ apps/standalone/src/pages/RedirectPage.tsx
@@ -1,12 +1,19 @@
 import { generatePath, Navigate } from "@moose-desk/core";
+import useAuth from "src/hooks/useAuth";
 import AgentRoutePaths from "src/modules/agent/routes/paths";
+import DashboardRoutePaths from "src/modules/dashboard/routes/paths";
 
 interface RedirectPageProps {
   children?: React.ReactElement | React.ReactNode;
 }
 
 const RedirectPage = ({ children }: RedirectPageProps) => {
-  return <Navigate to={generatePath(AgentRoutePaths.Login)} replace />;
+  const { isLoggedIn } = useAuth();
+  if (isLoggedIn) {
+    return <Navigate to={generatePath(DashboardRoutePaths.Index)} replace />;
+  } else {
+    return <Navigate to={generatePath(AgentRoutePaths.Login)} replace />;
+  }
 };
 
 export default RedirectPage;
