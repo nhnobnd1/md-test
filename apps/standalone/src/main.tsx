3377e5988e91cd6c2ceb173d63f0594638dbc844
laibavi <017laibavi.wts@gmail.com>
2023-01-06 14:49:21 +0700

    set env package

--- apps/standalone/src/main.tsx
+++ apps/standalone/src/main.tsx
@@ -6,15 +6,19 @@ import {
   TokenManager,
 } from "@moose-desk/core";
 import LazyComponent from "@moose-desk/core/components/LazyComponent";
+import { Env } from "@moose-desk/repo";
 import { lazy, Suspense } from "react";
 import { CookiesProvider } from "react-cookie";
 import ReactDOM from "react-dom";
 import { Loading } from "src/components/Loading";
+import env from "src/core/env";
 import ModuleLoader from "src/core/utilities/ModuleLoader";
 import ErrorBoundary from "src/ErrorBoundary";
 import("src/styles/tailwind.scss").then(() =>
   import("antd/dist/reset.css").then(() => import("src/styles/index.scss"))
 );
+
+Env.setApiUrl(env.API_URL);
 
 ReactDOM.render(
   <ErrorBoundary>
