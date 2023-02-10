0a6803c760811660c249e65c4f659f576d31bfbe
Vix <017laibavi.wts@gmail.com>
2023-02-10 14:26:55 +0700

    fix pagination

--- apps/standalone/src/components/UI/Pagination/Pagination.tsx
+++ apps/standalone/src/components/UI/Pagination/Pagination.tsx
@@ -2,13 +2,19 @@ import {
   Pagination as APagination,
   PaginationProps as APaginationProps,
 } from "antd";
-import { useCallback } from "react";
+import { useCallback, useMemo } from "react";
+import Select, { OptionType } from "src/components/UI/Select/Select";
 
 interface PaginationProps extends Omit<APaginationProps, "onChange"> {
   total: number;
   pageSize: number;
   currentPage: number;
   onChange: ({ page, limit }: { page: number; limit: number }) => void;
+}
+
+interface ShowSizeProps {
+  total: number;
+  options: OptionType[];
 }
 
 const Pagination = ({
@@ -18,6 +24,21 @@ const Pagination = ({
   onChange,
   ...props
 }: PaginationProps) => {
+  const pageSizeOptions = useMemo(() => {
+    if (props.pageSizeOptions) {
+      return props.pageSizeOptions.map((item) => ({
+        value: item,
+        label: String(item),
+      }));
+    } else {
+      return [
+        { value: 10, label: "10" },
+        { value: 20, label: "20" },
+        { value: 50, label: "50" },
+      ];
+    }
+  }, [props.pageSizeOptions]);
+
   const handleChangePagination = useCallback(
     (page: number, pageSize: number) => {
       onChange({ page, limit: pageSize });
@@ -32,13 +53,45 @@ const Pagination = ({
     [onChange]
   );
 
+  const handlePageSizeChange = useCallback(
+    (pageSize) => {
+      onChange({ page: currentPage, limit: pageSize });
+    },
+    [onChange]
+  );
+
+  const ShowSize = ({ total, options }: ShowSizeProps) => {
+    return (
+      <div className="flex items-center">
+        <span className="mr-2">{total} Results</span>
+        <Select
+          className="mr-2"
+          size={
+            !props.size
+              ? undefined
+              : props.size === "default"
+              ? "middle"
+              : "small"
+          }
+          value={pageSize ?? options[0].value}
+          options={options}
+          onChange={handlePageSizeChange}
+        ></Select>
+        <span>Records per page</span>
+      </div>
+    );
+  };
+
   return (
     <APagination
-      showTotal={(total, range) => `${range[0]}-${range[1]} of ${total} items`}
+      showTotal={(total, range) => (
+        <ShowSize total={total} options={pageSizeOptions} />
+      )}
       {...props}
       total={total}
       defaultPageSize={pageSize}
       pageSize={pageSize}
+      size={props.size ?? "default"}
       defaultCurrent={1}
       current={currentPage}
       onChange={handleChangePagination}
