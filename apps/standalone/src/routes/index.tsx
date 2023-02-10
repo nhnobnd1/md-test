e9b5c152ea2c9718392e3bbc9579f360f19b7913
Vix <017laibavi.wts@gmail.com>
2023-02-10 17:00:13 +0700

    fix bug 404 show routes

--- apps/standalone/src/routes/index.tsx
+++ apps/standalone/src/routes/index.tsx
@@ -9,15 +9,10 @@ import useAuth from "src/hooks/useAuth";
 import NotFound from "src/pages/NotFound";
 import RoutePaths from "src/routes/paths";
 
-import("./routes").then(({ default: newRoutes }) => {
-  RouterHandler.addRoute(...newRoutes);
-});
-
 const AppRoutesBased = () => {
   const { isLoggedIn } = useAuth();
   const [isRegisteredMiddleware, setIsRegisteredMiddleware] = useState(false);
   const { routes } = useRoutes();
-  console.log(routes);
 
   useEffect(() => {
     RouterHandler.registerMiddleware({
