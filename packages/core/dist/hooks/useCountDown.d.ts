75251d2ba1968aafc83c384b60287dfcb00ea0b6
Vix <017laibavi.wts@gmail.com>
2023-02-16 10:03:18 +0700

    code countdown

--- packages/core/dist/hooks/useCountDown.d.ts
+++ packages/core/dist/hooks/useCountDown.d.ts
@@ -6,5 +6,6 @@ export interface UseCountDown {
     state: number;
     clearCountDown: (key: string) => void;
     initCountdown: (key: string) => void;
+    checkTimerProcess: boolean;
 }
 export declare function useCountDown({ initValue, key }: CountDown): UseCountDown;
