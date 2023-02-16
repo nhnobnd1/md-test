5a6b2a6ba8977b158a60c88371901ddd50f20e5d
Vix <017laibavi.wts@gmail.com>
2023-02-16 15:49:05 +0700

    fix pagination pagesize default

--- apps/standalone/src/components/UI/Pagination/Pagination.tsx
+++ apps/standalone/src/components/UI/Pagination/Pagination.tsx
@@ -35,6 +35,7 @@ const Pagination = ({
         { value: 10, label: "10" },
         { value: 20, label: "20" },
         { value: 50, label: "50" },
+        { value: 100, label: "100" },
       ];
     }
   }, [props.pageSizeOptions]);
