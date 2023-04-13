7987db6620e03e5d4ae5db719ff31107d13b4f01
namnh <nhnobnd1@gmail.com>
2023-04-13 13:38:50 +0700

    add interface base token

--- packages/core/dist/utilities/useRole.d.ts
+++ packages/core/dist/utilities/useRole.d.ts
@@ -1,2 +1,26 @@
+interface BaseToken {
+    sub: string;
+    preferred_username: string;
+    email: string;
+    role: string;
+    tenantid: string;
+    given_name: string;
+    family_name: string;
+    phone_number: string;
+    phone_number_verified: string;
+    email_verified: string;
+    unique_name: string;
+    storeId: string;
+    subdomain: string;
+    oi_prst: string;
+    oi_au_id: string;
+    client_id: string;
+    oi_tkn_id: string;
+    scope: string;
+    exp: number;
+    iss: string;
+    iat: string;
+}
 export declare function useRole(): string;
-export declare function useUser(): unknown;
+export declare function useUser(): BaseToken | null;
+export {};
--- packages/core/utilities/useRole.ts
+++ packages/core/utilities/useRole.ts
@@ -1,5 +1,28 @@
 import jwt_decode from "jwt-decode";
 import { TokenManager } from "./StorageManager";
+interface BaseToken {
+  sub: string;
+  preferred_username: string;
+  email: string;
+  role: string;
+  tenantid: string;
+  given_name: string;
+  family_name: string;
+  phone_number: string;
+  phone_number_verified: string;
+  email_verified: string;
+  unique_name: string;
+  storeId: string;
+  subdomain: string;
+  oi_prst: string;
+  oi_au_id: string;
+  client_id: string;
+  oi_tkn_id: string;
+  scope: string;
+  exp: number;
+  iss: string;
+  iat: string;
+}
 
 export function useRole(): string {
   const baseToken = TokenManager.getToken("base_token");
@@ -13,7 +36,7 @@ export function useRole(): string {
 export function useUser() {
   const baseToken = TokenManager.getToken("base_token");
   if (baseToken) {
-    const decoded = jwt_decode(baseToken);
+    const decoded: BaseToken = jwt_decode(baseToken);
     return decoded;
   }
   return null;
