c9fb60b687398bb5c39b6f51ab734beafc2f5946
Truong Nguyen <truong.nguyen@moosedesk.com>
2023-04-16 23:16:18 +0700

    debug

--- apps/shopify/web/helpers/api-services.js
+++ apps/shopify/web/helpers/api-services.js
@@ -26,32 +26,32 @@ export function api() {
     },
 
     post: async (url, customConfig) => {
-      const response = await fetch(process.env.API_URL + url, {
-        ...config,
-        ...customConfig,
-        method: "POST",
-        body: JSON.stringify(customConfig.body),
-      });
-      console.log("process.env.API_URL + url: ", process.env.API_URL + url);
-      const data = await response.json();
-      return data;
+      try {
+        console.log("process.env.API_URL + url: ", process.env.API_URL + url);
+        const response = await fetch(process.env.API_URL + url, {
+          ...config,
+          ...customConfig,
+          method: "POST",
+          body: JSON.stringify(customConfig.body),
+        });
+        const data = await response.json();
+        return data;
+      } catch (e) {
+        console.log("error registerUser: ", e);
+      }
     },
   };
 }
 
 // list function api
 export async function registerUser(payload) {
-  try {
-    const response = await api().post("/v1/account/shopify/sign-up", {
-      body: payload,
-    });
-    if (response.statusCode === 200) {
-      console.log("sign up success", response);
-    } else {
-      console.log("sign up failed", response);
-    }
-    return response;
-  } catch (e) {
-    console.log("error registerUser: ", e);
+  const response = await api().post("/v1/account/shopify/sign-up", {
+    body: payload,
+  });
+  if (response.statusCode === 200) {
+    console.log("sign up success", response);
+  } else {
+    console.log("sign up failed", response);
   }
+  return response;
 }
