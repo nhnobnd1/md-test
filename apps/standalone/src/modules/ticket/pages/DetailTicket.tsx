b18b7cd7522cead2a1447f8f2f3d1ad5c703e3b4
vuong.cu <vuong.cu@moosedesk.com>
2023-04-17 11:17:17 +0700

    update auto fill date time picker

--- apps/standalone/src/modules/ticket/pages/DetailTicket.tsx
+++ apps/standalone/src/modules/ticket/pages/DetailTicket.tsx
@@ -1,6 +1,5 @@
 import { LeftCircleOutlined, RightCircleOutlined } from "@ant-design/icons";
 import useToggleGlobal from "@moose-desk/core/hooks/useToggleGlobal";
-import { Tooltip } from "antd";
 import classNames from "classnames";
 import { useEffect } from "react";
 import DetailTicketForm from "src/modules/ticket/components/DetailTicketForm/DetailTicketForm";
@@ -20,12 +19,12 @@ const DetailTicket = (props: DetailTicketProps) => {
   const _renderButtonToggle = () => {
     return !visible ? (
       <LeftCircleOutlined
-        className={styles.toggleButton}
+        className={classNames(styles.toggleButton, styles.toggleButtonOpen)}
         onClick={handleOpenDrawerSearch}
       />
     ) : (
       <RightCircleOutlined
-        className={styles.toggleButton}
+        className={classNames(styles.toggleButton, styles.toggleButtonClose)}
         onClick={handleCloseDrawerSearch}
       />
     );
@@ -38,11 +37,7 @@ const DetailTicket = (props: DetailTicketProps) => {
   return (
     <section className={classNames(styles.container, { "d-flex": visible })}>
       <div className={styles.wrapContent}>
-        <div className={styles.wrapSearchToggle}>
-          <Tooltip title={visible ? "Close" : "Search Order Shopify"}>
-            {_renderButtonToggle()}
-          </Tooltip>
-        </div>
+        <div className={styles.wrapSearchToggle}>{_renderButtonToggle()}</div>
         <DetailTicketForm />
       </div>
       <div className={visible ? "" : "d-none"}>
