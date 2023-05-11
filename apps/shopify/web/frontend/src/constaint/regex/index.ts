66255806bc6a0504870e7876df7b333f2f360be6
namnh <nhnobnd1@gmail.com>
2023-05-11 14:20:35 +0700

    update regex password shopify

--- apps/shopify/web/frontend/src/constaint/regex/index.ts
+++ apps/shopify/web/frontend/src/constaint/regex/index.ts
@@ -5,14 +5,14 @@ const validateSchemaObjectPassword = object().shape({
   currentPassword: string()
     .matches(
       // eslint-disable-next-line no-useless-escape
-      /^(?=.*[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*[@$!%*#=\/^?&])[a-zA-Z@$!%*#=\/^?&\d]{8,}$/g,
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$/,
       "The password must be 8 characters long and must be a combination of uppercase letters, lowercase letters, numbers, and symbols"
     )
     .required("The current password is required"),
   newPassword: string()
     .matches(
       // eslint-disable-next-line no-useless-escape
-      /^(?=.*[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*[@$!%*#=\/^?&])[a-zA-Z@$!%*#=\/^?&\d]{8,}$/g,
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$/,
       "The password must be 8 characters long and must be a combination of uppercase letters, lowercase letters, numbers, and symbols"
     )
     .required("New Password is required!"),
