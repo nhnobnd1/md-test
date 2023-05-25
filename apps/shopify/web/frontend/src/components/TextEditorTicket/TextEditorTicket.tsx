ba77f25d4457818272606ba5634fad9bda50d694
namnh <nhnobnd1@gmail.com>
2023-05-25 11:38:28 +0700

    fixed font rich editor

--- apps/shopify/web/frontend/src/components/TextEditorTicket/TextEditorTicket.tsx
+++ apps/shopify/web/frontend/src/components/TextEditorTicket/TextEditorTicket.tsx
@@ -251,8 +251,8 @@ export const TextEditorTicket = ({
           toolbar_mode: "sliding",
           fontsize_formats:
             "8pt 9pt 10pt 11pt 12pt 14pt 18pt 24pt 30pt 36pt 48pt 60pt 72pt 96pt",
-          content_style:
-            "body { font-family:Helvetica,Arial,sans-serif; font-size:12pt }",
+          // content_style:
+          //   "body { font-family:Helvetica,Arial,sans-serif; font-size:12pt }",
           toolbar:
             "undo redo | bold italic underline align | importfile | blocks fontfamily fontsize  | link code copy cut past blockquote backcolor forecolor indent newdocument lineheight selectall strikethrough",
           plugins: [
--- apps/standalone/src/components/UI/Editor/TextEditor.tsx
+++ apps/standalone/src/components/UI/Editor/TextEditor.tsx
@@ -48,8 +48,8 @@ const TextEditor = ({
           toolbar_mode: "sliding",
           fontsize_formats:
             "8pt 9pt 10pt 11pt 12pt 14pt 18pt 24pt 30pt 36pt 48pt 60pt 72pt 96pt",
-          content_style:
-            "body { font-family:Helvetica,Arial,sans-serif; font-size:12pt }",
+          // content_style:
+          //   "body { font-family:Helvetica,Arial,sans-serif; font-size:12pt }",
           toolbar:
             "undo redo | bold italic underline align | blocks fontfamily fontsize | link image code copy cut past blockquote backcolor forecolor indent newdocument lineheight selectall strikethrough",
           plugins: [
