be196eb7001f9391f3809aadcba2181ee9363024
namnh <nhnobnd1@gmail.com>
2023-06-13 15:28:04 +0700

    change breakpoint file

--- apps/shopify/web/frontend/src/hooks/useScreenType.ts
+++ apps/shopify/web/frontend/src/hooks/useScreenType.ts
@@ -11,8 +11,8 @@ type Breakpoints = {
 const defaultBreakpoints: Breakpoints = {
   sm: "320px",
   md: "768px",
-  lg: "992px",
-  xl: "1200px",
+  lg: "1024px",
+  xl: "1280px",
   xxl: "1600px",
 };
 
