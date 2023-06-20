2bde577024bd6f9da6ca9f5d6d9db597aa853efc
vuong.cu <vuong.cu@moosedesk.com>
2023-06-20 13:49:52 +0700

    feat ui login and fix bug resend otp

--- apps/standalone/src/modules/agent/pages/auth/forgotPassword/ForgotPassword.tsx
+++ apps/standalone/src/modules/agent/pages/auth/forgotPassword/ForgotPassword.tsx
@@ -5,12 +5,13 @@ import { useCallback, useState } from "react";
 import ReCAPTCHA from "react-google-recaptcha";
 import { useTranslation } from "react-i18next";
 import { catchError, map, of } from "rxjs";
+import logo from "src/assets/images/logo/logoBase.png";
 import env from "src/core/env";
 import useMessage from "src/hooks/useMessage";
 import useNotification from "src/hooks/useNotification";
 import { useStore } from "src/providers/StoreProviders";
 import RoutePaths from "src/routes/paths";
-import "./ForgotPassword.scss";
+import styles from "./styles.module.scss";
 
 export const ForgotPassword = () => {
   const [form] = Form.useForm();
@@ -59,91 +60,84 @@ export const ForgotPassword = () => {
   }, []);
 
   return (
-    <div className="forgotPassword">
-      <div className="card-forgot-password">
-        <div className="pt-[10%]">
-          <h2 className="text-center mb-[40px]">Reset your password</h2>
-          {!finalPage.status ? (
-            <>
-              <div className="text-center mb-6">
-                Please enter your email address and we will send the
-                instructions to reset your password
-              </div>
-              <div>
-                <Form
-                  layout="horizontal"
-                  labelCol={{ span: 4 }}
-                  wrapperCol={{ span: 20 }}
-                  form={form}
-                  onFinish={handleFinish}
+    <section className={styles.container}>
+      <div className={styles.loginWrap}>
+        <img className={styles.logo} src={logo} alt="logo" />
+        <h2>Reset your password</h2>
+        {!finalPage.status ? (
+          <>
+            <p className={styles.subHeader}>
+              Please enter your email address and we will send the instructions
+              to reset your password
+            </p>
+            <div className={styles.formWrap}>
+              <Form layout="vertical" form={form} onFinish={handleFinish}>
+                <Form.Item
+                  label="Email"
+                  name="email"
+                  rules={[
+                    { required: true, message: "Email address is required" },
+                    {
+                      type: "email",
+                      message: "The email address is not valid",
+                    },
+                  ]}
                 >
+                  <Input type="text" placeholder="Enter Email" size="large" />
+                </Form.Item>
+                <div className="flex">
                   <Form.Item
-                    label="Email"
-                    name="email"
+                    className="w-full hide-label"
+                    name="captcha"
+                    label="captcha"
                     rules={[
-                      { required: true, message: "Email address is required" },
-                      {
-                        type: "email",
-                        message: "The email address is not valid",
-                      },
+                      { required: true, message: "Please enter captcha" },
                     ]}
                   >
-                    <Input type="text" />
+                    <ReCAPTCHA
+                      className={styles.reCaptcha}
+                      sitekey={env.RECAPTCHA_KEYS}
+                    ></ReCAPTCHA>
                   </Form.Item>
-                  <div className="flex">
-                    <Form.Item
-                      className="w-full hide-label"
-                      name="captcha"
-                      label="captcha"
-                      rules={[
-                        { required: true, message: "Please enter captcha" },
-                      ]}
-                    >
-                      <ReCAPTCHA
-                        className="capt-cha"
-                        sitekey={env.RECAPTCHA_KEYS}
-                      ></ReCAPTCHA>
-                    </Form.Item>
-                  </div>
-                  <div className="text-center">
-                    <Button onClick={() => form.submit()} type="primary">
-                      Submit
-                    </Button>
-                  </div>
-                </Form>
-              </div>
-            </>
-          ) : (
-            <>
-              {finalPage.isSuccess ? (
-                <>
-                  <div className="mb-2 font-medium">
-                    We have sent the instructions to your email address.
-                  </div>
-                  <div className="mb-8 font-medium">
-                    Please follow the instructions in order to reset your
-                    password.
-                  </div>
-                </>
-              ) : (
-                <div className="text-center font-medium mb-8">
-                  The system failed to send email to your email address, please
-                  enter a valid email address.
                 </div>
-              )}
-              <div className="text-center">
-                <span
-                  className="link"
-                  onClick={() => navigate(generatePath(RoutePaths.Login))}
-                >
-                  Back to login page
-                </span>
+                <div className="text-center">
+                  <Button onClick={() => form.submit()} type="primary">
+                    Submit
+                  </Button>
+                </div>
+              </Form>
+            </div>
+          </>
+        ) : (
+          <>
+            {finalPage.isSuccess ? (
+              <>
+                <div className="mb-2 font-medium">
+                  We have sent the instructions to your email address.
+                </div>
+                <div className="mb-8 font-medium">
+                  Please follow the instructions in order to reset your
+                  password.
+                </div>
+              </>
+            ) : (
+              <div className="text-center font-medium mb-8">
+                The system failed to send email to your email address, please
+                enter a valid email address.
               </div>
-            </>
-          )}
-        </div>
+            )}
+            <div className="text-center">
+              <span
+                className="link"
+                onClick={() => navigate(generatePath(RoutePaths.Login))}
+              >
+                Back to login page
+              </span>
+            </div>
+          </>
+        )}
       </div>
-    </div>
+    </section>
   );
 };
 
