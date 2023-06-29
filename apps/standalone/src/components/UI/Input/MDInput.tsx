a1bb598f1e509701036a4368a35067778d0c351c
namnh <nhnobnd1@gmail.com>
2023-06-29 17:49:25 +0700

    update height input

--- apps/standalone/src/components/UI/Input/MDInput.tsx
+++ apps/standalone/src/components/UI/Input/MDInput.tsx
@@ -1,12 +1,13 @@
 import { Input, InputProps } from "antd";
 import useViewport from "src/hooks/useViewport";
 import styles from "./style.module.scss";
+import classNames from "classnames";
 interface IMDInput extends InputProps {}
 export const MDInput = ({ ...props }: IMDInput) => {
   const { isMobile } = useViewport();
   return (
     <Input
-      className={styles.MDInput}
+      className={classNames(styles.MDInput, `${!isMobile && "h-[40px]"}`)}
       size={isMobile ? "middle" : "large"}
       {...props}
     />
