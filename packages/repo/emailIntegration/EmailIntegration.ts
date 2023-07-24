4f78021c3602e6ad69b5641ced8685adc876d57f
namnh <nhnobnd1@gmail.com>
2023-07-24 11:20:27 +0700

    update

--- packages/repo/emailIntegration/EmailIntegration.ts
+++ packages/repo/emailIntegration/EmailIntegration.ts
@@ -103,7 +103,7 @@ export interface CreateEmailIntegrationRequest {
 	isPrimaryEmail: boolean;
 	mailboxType: MailBoxType;
 	mailboxConfig: MailBoxConfig | { forwardEmail: string };
-	signature: string;
+	signature: string | undefined;
 }
 
 export type CreateEmailIntegrationResponse = BaseResponse<EmailIntegration>;
