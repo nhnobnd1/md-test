baa785a94d7ea1cc5c8461b933b5a04455a09512
namnh <nhnobnd1@gmail.com>
2023-07-24 16:12:18 +0700

    update row message

--- apps/standalone/src/modules/ticket/components/DetailTicketForm/RowMessage.tsx
+++ apps/standalone/src/modules/ticket/components/DetailTicketForm/RowMessage.tsx
@@ -1,9 +1,7 @@
 import { Button, Collapse, Popover } from "antd";
 import { filesize } from "filesize";
-import parse, { Element } from "html-react-parser";
 import { FC, useMemo, useState } from "react";
 import { ChatItem } from "src/modules/ticket/components/DetailTicketForm/DetailTicketForm";
-import ImageZoom from "src/modules/ticket/components/DetailTicketForm/ImageZoom";
 import UserIcon from "~icons/material-symbols/person";
 import AgentIcon from "~icons/material-symbols/support-agent-sharp";
 import QuoteIcon from "~icons/octicon/ellipsis-16";
@@ -25,27 +23,7 @@ function splitText(fileName: string, maxLength: number) {
   return shortName;
 }
 
-const regexQuote = /<div class="md_quote">[\s\S]*?<\/blockquote>/;
-
 const regexContent = /^.*(?=<div class="md_quote">)/s;
-
-const parseHtml = (html: string): React.ReactNode => {
-  const options: any = {
-    replace: (domNode: Element): React.ReactNode => {
-      if (domNode.name === "img") {
-        return (
-          <ImageZoom
-            key={domNode.attribs.src}
-            src={domNode.attribs.src}
-            alt={domNode.attribs.alt}
-          />
-        );
-      }
-      return undefined; // Return undefined if we don't want to replace the node
-    },
-  };
-  return parse(html, options);
-};
 
 export const RowMessage: FC<RowMessageProps> = ({ item }) => {
   const [toggleQuote, setToggleQuote] = useState(true);
@@ -58,10 +36,11 @@ export const RowMessage: FC<RowMessageProps> = ({ item }) => {
   }, [item.chat]);
 
   const quote = useMemo(() => {
-    if (item.chat.match(regexQuote)) {
-      return item.chat.match(regexQuote)?.[0] as string;
+    const startIndex = item.chat.indexOf('<div class="md_quote">');
+    if (startIndex !== -1) {
+      const remainingHTML = item.chat.slice(startIndex);
+      return remainingHTML;
     }
-
     return "";
   }, [item.chat]);
 
@@ -75,7 +54,7 @@ export const RowMessage: FC<RowMessageProps> = ({ item }) => {
   return (
     <div className="">
       {/* <style scoped>{css}</style> */}
-      <div className=" items-center gap-3 mx-2">
+      <div className=" items-center gap-3 ">
         <div className="flex items-end gap-3 ">
           {/* <h2 style={{ color: "black", margin: 0 }}>{item.name}</h2> */}
           {item?.incoming ? (
@@ -140,7 +119,7 @@ export const RowMessage: FC<RowMessageProps> = ({ item }) => {
         </div>
       </div>
       <div
-        className="text-black text-scroll mt-5"
+        className="text-black text-scroll mt-5 "
         dangerouslySetInnerHTML={{
           __html: `<div style="font-family:Helvetica;font-size:14px">${sortChat}</div>`,
         }}
@@ -176,7 +155,7 @@ export const RowMessage: FC<RowMessageProps> = ({ item }) => {
       {disableQuote ? (
         <></>
       ) : (
-        <div className="mt-10">
+        <div className="">
           <Popover content={<>Quote</>} className="mt-2 mb-5">
             <Button
               type="text"
