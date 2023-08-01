a67b2b73e7ab981d1573b93c72cbcc60e5a6bf86
namnh <nhnobnd1@gmail.com>
2023-08-01 17:29:03 +0700

    move moment-timezone to dayjs

--- packages/core/utilities/convertTime.ts
+++ packages/core/utilities/convertTime.ts
@@ -1,11 +1,20 @@
-import moment from "moment-timezone";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
 const createdDatetimeFormat = (
   time: string | null | undefined,
   timezone: null | string = null
 ) => {
   if (timezone) {
-    return time ? moment(time).tz(timezone).format("MM/DD/YYYY HH:mm:ss") : "";
+    return time
+      ? dayjs.utc(time).tz(timezone).format("MM/DD/YYYY HH:mm:ss")
+      : "";
   }
-  return time ? moment(time).format("MM/DD/YYYY HH:mm:ss") : "";
+  return time ? dayjs(time).format("MM/DD/YYYY HH:mm:ss") : "";
 };
+
 export { createdDatetimeFormat };
