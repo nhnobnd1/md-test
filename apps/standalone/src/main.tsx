3e1ef868064ca10275b2472b8954107a6104c500
namnh <nhnobnd1@gmail.com>
2023-08-01 18:01:04 +0700

    disable sentry

--- apps/standalone/src/main.tsx
+++ apps/standalone/src/main.tsx
@@ -6,20 +6,17 @@ import {
 } from "@moose-desk/core";
 import LazyComponent from "@moose-desk/core/components/LazyComponent";
 import { AccountRepository, Env } from "@moose-desk/repo";
-import * as Sentry from "@sentry/react";
 import { Suspense, lazy } from "react";
 import ReactDOM from "react-dom";
 import ReactGA from "react-ga4";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { createWebStoragePersistor } from "react-query/createWebStoragePersistor-experimental";
-import { persistQueryClient } from "react-query/persistQueryClient-experimental";
 import { Loading } from "src/components/Loading";
 import env from "src/core/env";
 import ModuleLoader from "src/core/utilities/ModuleLoader";
 import AppConfigProviders from "src/providers/AppConfigProviders";
 import InitApp from "src/providers/InitAppProviders";
 import { StoreProviders } from "src/providers/StoreProviders";
-import { sentryRegex } from "src/regex";
 import { getBaseToken, getRefreshToken } from "src/utils/localValue";
 import("src/styles/tailwind.scss").then(() =>
   import("antd/dist/reset.css").then(() => import("src/styles/index.scss"))
@@ -43,25 +40,25 @@ const localStoragePersistor = createWebStoragePersistor({
 //   persistor: localStoragePersistor,
 // });
 ReactGA.initialize(env.TRACKING_ID);
-Sentry.init({
-  dsn: env.DSN_SENTRY,
-  beforeSend(event) {
-    // console.log("{ event }", event.request);
-    if (sentryRegex.test(event.request?.url as string)) return event;
-    return null;
-  },
-  integrations: [
-    new Sentry.BrowserTracing({
-      tracePropagationTargets: [/^https:\/\/([\w-]+\.)+moosedesk\.net$/],
-    }),
-    new Sentry.Replay(),
-  ],
-  // Performance Monitoring
-  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-});
+// Sentry.init({
+//   dsn: env.DSN_SENTRY,
+//   beforeSend(event) {
+//     // console.log("{ event }", event.request);
+//     if (sentryRegex.test(event.request?.url as string)) return event;
+//     return null;
+//   },
+//   integrations: [
+//     new Sentry.BrowserTracing({
+//       tracePropagationTargets: [/^https:\/\/([\w-]+\.)+moosedesk\.net$/],
+//     }),
+//     new Sentry.Replay(),
+//   ],
+//   // Performance Monitoring
+//   tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+//   // Session Replay
+//   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+//   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+// });
 ReactDOM.render(
   <BrowserRouter>
     <QueryClientProvider client={queryClient}>
