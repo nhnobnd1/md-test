0b3193be82e721432bc50f85ecd7cfe843a33047
vuong.cu <vuong.cu@moosedesk.com>
2023-08-09 23:35:30 +0700

    update/setup

--- apps/shopify/web/frontend/src/modules/settingChannel/components/widgets/Setup/Setup.tsx
+++ apps/shopify/web/frontend/src/modules/settingChannel/components/widgets/Setup/Setup.tsx
@@ -1,111 +1,29 @@
-import { Link, Select, Text } from "@shopify/polaris";
-import classNames from "classnames";
-import { useCallback, useMemo, useState } from "react";
-import { useQuery } from "react-query";
-import { useSubdomain } from "src/hooks/useSubdomain";
-import { getListTheme } from "src/modules/settingChannel/components/widgets/Setup/api/api";
-import { Theme } from "src/modules/settingChannel/components/widgets/Setup/helper/interface";
+import { ChoiceList } from "@shopify/polaris";
+import { useCallback, useState } from "react";
+import { Automation } from "src/modules/settingChannel/components/widgets/Setup/components/Automation";
+import { Manual } from "src/modules/settingChannel/components/widgets/Setup/components/Manual";
 import styles from "./setup.module.scss";
 export default function Setup() {
-  const { getSubDomain } = useSubdomain();
-  const [themeId, setThemeId] = useState<string>("current");
-
-  const { data, isLoading }: any = useQuery({
-    queryKey: ["themeList"],
-    queryFn: () => getListTheme(),
-  });
-  const deselectedOptions = useMemo(() => {
-    if (!data?.data?.data?.length) return [];
-    return data?.data?.data?.map((theme: Theme) => {
-      return {
-        value: String(theme.id),
-        label: theme.name,
-      };
-    });
-  }, [data?.data?.data]);
-  const handleSelectChange = useCallback(
-    (value: string) => setThemeId(value),
-    []
-  );
+  const [type, setType] = useState(["automation"]);
+  const handleChange = useCallback((value: string[]) => setType(value), []);
+  console.log(type[0]);
   return (
     <div className={styles.container}>
-      {/* <Card sectioned> */}
-      <div className={styles.title}>
-        <Text variant="headingMd" as="h2">
-          Automation setup
-        </Text>
-      </div>
-      <div className={styles.wrapThemeSelect}>
-        <Select
-          label="Step 1: Select theme"
-          options={deselectedOptions}
-          onChange={handleSelectChange}
-          value={themeId}
-        />
-      </div>
       <div>
-        <p>Step 2: Click the button below to go to the Theme Editor</p>
-        <div
-          className={classNames(
-            styles.buttonLink,
-            "Polaris-Button Polaris-Button--primary"
-          )}
-        >
-          <Link
-            removeUnderline
-            url={`https://${getSubDomain()}.myshopify.com/admin/themes/${themeId}/editor?template=index&addAppBlockId=ed118f6f-db02-4570-8695-4416c857ded1/app-widget&target=sectionGroup:footer`}
-          >
-            Go to Theme Editor
-          </Link>
-        </div>
-        <p>Step 3: Click Save</p>
-        <img
-          src={
-            "https://cdn.shopify.com/s/files/1/0805/0819/1024/files/md-setup-widget-auto-1.png?v=1691587731"
-          }
+        {" "}
+        <ChoiceList
+          title="Choose setup method"
+          choices={[
+            { label: "Automation", value: "automation" },
+            { label: "Manual", value: "manual" },
+          ]}
+          selected={type}
+          onChange={handleChange}
         />
       </div>
-      <div className={styles.tutorial}>
-        <div className={styles.title}>
-          {" "}
-          <Text variant="headingMd" as="h2">
-            Manual setup
-          </Text>
-        </div>
-
-        <p>
-          - Step 1: From Shopify admin, go to <span>Online Store</span> &#62;{" "}
-          <span>Themes</span> &#62; <span>Customize</span>
-        </p>
-        <img
-          src={
-            "https://cdn.shopify.com/s/files/1/0805/0819/1024/files/md-setup-widget-manual-step-1.png?v=1691587730"
-          }
-        />
-        <p>
-          - Step 2: From the <span>Sections</span> at the left top, click{" "}
-          <span>Add section</span> in <span>Footer</span>
-        </p>
-        <p>
-          - Step 3: Click <span>Apps</span> tab, and select{" "}
-          <span>MooseDesk Help Widget</span>
-        </p>
-        <img
-          src={
-            "https://cdn.shopify.com/s/files/1/0805/0819/1024/files/md-setup-widget-manual-step-2.png?v=1691587730"
-          }
-        />
-        <p>
-          - Step 4: Click <span>Save</span>
-        </p>
-        <img
-          src={
-            "https://cdn.shopify.com/s/files/1/0805/0819/1024/files/md-setup-widget-manual-step-3.png?v=1691587731"
-          }
-        />
+      <div className={styles.content}>
+        {type[0] === "automation" ? <Automation /> : <Manual />}
       </div>
-
-      {/* </Card> */}
     </div>
   );
 }
